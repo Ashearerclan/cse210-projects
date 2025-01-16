27cbba7738c9cc2b0bad65063bbd6288d91c5736
Ayden Shearer <aydenshearer@icloud.com>
2025-01-16 16:19:09 -0700

    Updated Sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // This is a comment
+        string adjective = GetAdjective();
+        string noun = GetNoun();
+
+        int number = Multiply(3, 4);
+
+        Console.WriteLine($"I looked out the window and saw {number} {adjective} {noun}s");
+    }
+
+    static int Multiply(int number1, int number2)
+    {
+        int product = number1 * number2;
+
+        return product;
+    }
+
+    static string GetAdjective()
+    {
+        List<string> words = new List<string>();
+        words.Add("blue");
+        words.Add("yellow");
+        words.Add("big");
+        words.Add("small");
+
+        string adjective = words[2];
+
+        return adjective;
+    }
+
+    static string GetNoun()
+    {
+        string word = "bird";
+
+        return word;
     }
 }
