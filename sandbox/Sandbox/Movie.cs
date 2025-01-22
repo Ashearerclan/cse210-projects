1de61ac974c4b025559818e9f1406e2796702018
Ayden Shearer <aydenshearer@icloud.com>
2025-01-21 18:00:25 -0700

    Sandbox Changes

---
+++ sandbox/Sandbox/Movie.cs
@@ -0,0 +1,7 @@
+public class Movie
+{
+    public string _title;
+    public int _year;
+    public int _runtime;
+    public string _rating;
+}
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,42 +1,23 @@
 using System;
+using System.Formats.Tar;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // This is a comment
-        string adjective = GetAdjective();
-        string noun = GetNoun();
+        int name = "John";
 
-        int number = Multiply(3, 4);
+        Movie favoriteMovie = new Movie();
+        favoriteMovie._title = "Star Wars";
+        favoriteMovie._year = 1977;
+        favoriteMovie._rating = "PG";
+        favoriteMovie._runtime = 150;
 
-        Console.WriteLine($"I looked out the window and saw {number} {adjective} {noun}s");
-    }
-
-    static int Multiply(int number1, int number2)
-    {
-        int product = number1 * number2;
-
-        return product;
-    }
-
-    static string GetAdjective()
-    {
-        List<string> words = new List<string>();
-        words.Add("blue");
-        words.Add("yellow");
-        words.Add("big");
-        words.Add("small");
-
-        string adjective = words[2];
-
-        return adjective;
-    }
-
-    static string GetNoun()
-    {
-        string word = "bird";
-
-        return word;
+        Movie otherMovie = new Movie();
+        otherMovie._title = "Avatar";
+        otherMovie._year = 2009;
+        otherMovie._rating = "PG-13";
+        otherMovie._runtime = 162;
     }
 }
