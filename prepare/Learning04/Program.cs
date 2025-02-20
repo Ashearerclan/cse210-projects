654625bbe3b2533d595566de4c1d50c5d5061104
Ayden Shearer <aydenshearer@icloud.com>
2025-02-19 20:42:39 -0700

    Completed W07 Prepare Learning Activity Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Creates base Assignment object
+        Assignment a1 = new Assignment("John Adams", "Writing");
+        Console.WriteLine(a1.GetSummary());
+
+        // Creates derived class assignments
+        MathAssignment a2 = new MathAssignment("John Brown", "Multiplication", "10.2", "1-10");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("John Mclain", "US History", "The American Civil Wars Battle Tactics");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
